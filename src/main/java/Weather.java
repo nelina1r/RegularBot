002d1fb49845b005cda976b9ec4f911d5fc238c5
Alexnder Dedov <dedov.al@bk.ru>
2021-07-16 22:16:57 +0300

    enough

---
+++ src/main/java/Weather.java
@@ -0,0 +1,39 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+    public static String getWeather(String message, Model model) throws IOException {
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=5738822268edd21f9a5d5b97a23c0076");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+        model.setHumidity(main.getDouble("humidity"));
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String) obj.get("icon"));
+            model.setMain((String) obj.get("main"));
+        }
+
+        return "Город: " + model.getName() + "\n" +
+                "Температура: " + model.getTemp() + "C" + "\n" +
+                "Влажность:" + model.getHumidity() + "%" + "\n" +
+                "Небо: " + model.getMain() + "\n" +
+                "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+    }
+}
